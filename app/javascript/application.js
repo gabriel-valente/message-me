d601f1a1c680699638a2f3d9c15218c21ba2c831
Gabriel Valente <gabrielmcv1@gmail.com>
2022-06-27 15:06:03 +0100

    Add js, favicon, semantic-ui. New navbar, sessions, chatroom views.

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import 'jquery';
+import 'semantic-ui';
+import '@hotwired/turbo-rails';
+import 'controllers';
+
+$(document).on('turbo:load', function () {
+	$('.ui.dropdown').dropdown();
+});
